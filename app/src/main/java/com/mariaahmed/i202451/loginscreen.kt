1cc85cc74b62571bbfc37125c3982b47688e72b5
MariaAhmed01 <mariaahmed2413@gmail.com>
2024-03-21 00:49:01 +0500

    login

--- app/src/main/java/com/mariaahmed/i202451/loginscreen.kt
+++ app/src/main/java/com/mariaahmed/i202451/loginscreen.kt
@@ -3,19 +3,38 @@ package com.mariaahmed.i202451
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.mariaahmed.i202451.Signup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlin.random.Random
 
 class loginscreen : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginscreen)
 
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val email = findViewById<TextView>(R.id.email)
+        var  pass = findViewById<TextView>(R.id.password)
         val forgotPasswordTextView = findViewById<TextView>(R.id.forgotpassword)
         val signUpTextView = findViewById<TextView>(R.id.signup_button)
         val loginbutton = findViewById<Button>(R.id.loginbutton)
+
 
         forgotPasswordTextView.setOnClickListener {
             OnforgotpasswordClicked()
@@ -26,8 +45,25 @@ class loginscreen : AppCompatActivity() {
         }
 
         loginbutton.setOnClickListener {
-            loginbuttonclicked()
+            var emailTxt = email.text.toString()
+            var passTxt = pass.text.toString()
+
+            //if password field is empty
+            if(emailTxt.isEmpty()) {
+                email.error= "Please enter email"
+                email.requestFocus()
+                return@setOnClickListener
+            }
+            if(passTxt.isEmpty()) {
+                pass.error= "Please enter password"
+                pass.requestFocus()
+                return@setOnClickListener
+            }
+
+            loginbuttonclicked(emailTxt, passTxt)
         }
+
+
 
     }
 
@@ -43,10 +79,30 @@ class loginscreen : AppCompatActivity() {
         startActivity(signupintent)
     }
 
-    fun loginbuttonclicked(){
+    fun loginbuttonclicked(email:String,pass:String){
 
-        val loginintent = Intent(this, dashboard::class.java)
-        startActivity(loginintent)
+        mAuth.signInWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithEmail:success")
+                    val user = mAuth.currentUser
+
+                    /* var secondActivityIntent = Intent(this, SecondActivity::class.java)
+                     startActivity(secondActivityIntent)*/
+
+                    val loginintent = Intent(this, dashboard::class.java)
+                    startActivity(loginintent)
+                    finish()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(this,"User name or Password is incorrect",Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
     }
 
 }
